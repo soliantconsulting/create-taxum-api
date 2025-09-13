cf58758a4fd97ee22bd2fb25b27f21a23e4077f4
Ben Scholzen (DASPRiD) <mail@dasprids.de>
2025-09-13 14:05:57 +0200

    feat(skeleton): update to taxum 1.0.0

--- skeleton/packages/api/src/route/articles.ts
+++ skeleton/packages/api/src/route/articles.ts
@@ -1,10 +1,10 @@
 import { JsonApiError } from "@jsonapi-serde/server/common";
 import { pathParam } from "@taxum/core/extract";
-import { extractHandler, m, Router } from "@taxum/core/routing";
+import { createExtractHandler, m, Router } from "@taxum/core/routing";
 import { z } from "zod";
 import { serialize } from "../json-api/index.js";
 
-const listHandler = extractHandler(() => {
+const listHandler = createExtractHandler().handler(() => {
     return serialize("article", [
         {
             id: "9018b265-9d14-403d-850d-968f09fae999",
@@ -13,7 +13,7 @@ const listHandler = extractHandler(() => {
     ]);
 });
 
-const showHandler = extractHandler(pathParam(z.uuid()), (articleId) => {
+const showHandler = createExtractHandler(pathParam(z.uuid())).handler((articleId) => {
     if (articleId !== "9018b265-9d14-403d-850d-968f09fae999") {
         throw new JsonApiError({ status: "404" });
     }
